666ca6dc93c5934155319148c5d86478acb51ded
Francesco Risso <francesco@frisso.net>
2023-04-21 12:45:24 +0200

    Cleaned the routing from the testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,19 +21,7 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 import "./App.css";
-import PresenterQuestionPage from "./pages/PresenterQuestionPage";
-import UserQuestionPage from "./pages/UserQuestionPage";
-import UserResultPage from "./pages/UserResultPage";
-import PresenterResultPage from "./pages/PresenterResultPage";
-import PresenterRankingPage from "./pages/PresenterLeaderboardPage";
-import RequirePassword from "./components/RequirePassword";
-import UserLoginPage from "./pages/UserRegisterPage";
 import { useState } from "react";
-import UserWaitingPage from "./pages/UserWaitingPage";
-import PresenterWordsRecap from "./pages/PresenterWordsRecap";
-import PresenterCountdownPage from "./pages/CountdownPage";
-import PresenterTextPage from "./pages/PresenterTextPage";
-import AdminPage from "./pages/AdminPage";
 import ContextProvider from "./components/Context";
 import ConnectionPage from "./pages/ConnectionPage";
 
@@ -60,170 +48,20 @@ const App: React.FC = () => {
 								<ConnectionPage userType="admin" />
 							</Route>
 
-							<Route exact path="/presenterQuestion">
-								<PresenterQuestionPage
-									answers={{
-										red: "A",
-										blue: "B",
-										yellow: "C",
-										green: "D"
-									}}
-									remainingTime={12}
-									submittedAnswers={23}
-									totalUsers={30}
-								/>
+							<Route exact path="/presenterTesting">
+								<ConnectionPage userType="presenter" testingButtons />
 							</Route>
 
-							<Route exact path="/userQuestion">
-								<UserQuestionPage
-									answers={{
-										red: "A",
-										blue: "B",
-										yellow: "C",
-										green: "D"
-									}}
-									remainingTime={12}
-									username="Username"
-									score={25}
-								/>
+							<Route exact path="/userTesting">
+								<ConnectionPage userType="user" testingButtons />
 							</Route>
 
-							<Route exact path="/userResult">
-								<UserResultPage thisScore={5} totScore={25} pos={4} username="Username" />
-							</Route>
-
-							<Route exact path="/presenterResult">
-								<PresenterResultPage
-									votes={{
-										red: 4,
-										yellow: 3,
-										blue: 6,
-										green: 8
-									}}
-									answers={{
-										red: "A",
-										blue: "B",
-										yellow: "C",
-										green: "D"
-									}}
-									correctVote="yellow"
-								/>
-							</Route>
-
-							<Route exact path="/presenterRanking">
-								<PresenterRankingPage
-									ranking={[
-										"Prova",
-										"Ciao",
-										"Utente",
-										"Pippo",
-										"Mario",
-										"Tizio",
-										"Caio",
-										"Sempronio",
-										"Buh",
-										"Test",
-										"Ultimo",
-										"Prova nome lungo",
-										"Help",
-										"Nome123",
-										"Quindicesimo",
-										"Prova",
-										"Ciao",
-										"Utente",
-										"Pippo",
-										"Mario",
-										"Tizio",
-										"Caio",
-										"Sempronio",
-										"Buh",
-										"Test",
-										"Ultimo",
-										"==> Max 22 caratteri <==",
-										"Help",
-										"Nome123",
-										"Non più quindicesimo"
-									]}
-								/>
-							</Route>
-
-							<Route exact path="/pwd">
-								<RequirePassword
-									password="prova"
-									content={<UserResultPage thisScore={5} totScore={25} pos={4} username="Username" />}
-								/>
-							</Route>
-
-							<Route exact path="/userLogin">
-								<UserLoginPage
-									username={username}
-									onUsernameChange={setUsername}
-									usernameTaken={false}
-								/>
-							</Route>
-
-							<Route exact path="/userWaitStart">
-								<UserWaitingPage message="Attendi che il gioco inizi..." spinner />
-							</Route>
-
-							<Route exact path="/presenterWordsRecap">
-								<PresenterWordsRecap
-									words={[
-										"Albero",
-										"Banana",
-										"Cavolfiore",
-										"Dattero",
-										"Emmental",
-										"Fiore di zucca",
-										"Gelato",
-										"Hamburger",
-										"Insalata",
-										"Jalapeno",
-										"Kiwi",
-										"Limone",
-										"Mango",
-										"Noce",
-										"Papaya",
-										"Quiche",
-										"Risotto",
-										"Sugo",
-										"Tonno",
-										"Uva",
-										"Valeriana",
-										"Wafer",
-										"X mi manca",
-										"Yogurt",
-										"Zucca"
-									]}
-								/>
-							</Route>
-
-							<Route exact path="/presenterCountDown">
-								<PresenterCountdownPage timeLeft={3} device="desktop" />
-							</Route>
-
-							<Route exact path="/userCountDown">
-								<PresenterCountdownPage timeLeft={3} device="mobile" />
-							</Route>
-
-							<Route exact path="/presenterTextPage">
-								<PresenterTextPage text={["Prova", "Altro test"]} />
-							</Route>
-
-							<Route exact path="/adminPage">
-								<AdminPage
-									allWords={Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ").map((l) => [
-										`${l}0`,
-										`${l}1`,
-										`${l}2`,
-										`${l}3`
-									])}
-									device="tablet"
-								/>
+							<Route exact path="/adminTesting">
+								<ConnectionPage userType="admin" testingButtons />
 							</Route>
 
 							<Route>
-								<Redirect to="/admin" />
+								<Redirect to="/user" />
 							</Route>
 						</IonRouterOutlet>
 					</IonReactRouter>
